0b84588c2dfdce60a3d9ca99b88accb96ad7dc20
Sarah Jacobs <sarah.jacobs1@student.ehb.be>
2019-05-20 10:19:12 +0200

    inlog

---
+++ Eventure/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Eventure
+//
+//  Created by Sarah Jacobs on 18/05/2019.
+//  Copyright © 2019 Sarah Jacobs. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var labelColours: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
