ce85fade9fcc36f7a6ceb5fa812f8984765e35cf
Sarah Jacobs <sarah.jacobs1@student.ehb.be>
2019-05-20 11:06:17 +0200

    Icons

--- Eventure/LoginViewController.swift
+++ Eventure/LoginViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
+
+
+
 class LoginViewController: UIViewController {
 
     @IBOutlet var labelColours: UILabel!
+    @IBOutlet var gebruikerIcon: UIImageView!
+    @IBOutlet var wachtwoordIcon: UIImageView!
+    
+    // https://stackoverflow.com/questions/27728466/use-multiple-font-colors-in-a-single-label/27728516
+    
+    var sloganString:NSString = "enjoy. report. earn."
+    var sloganMutableString = NSMutableAttributedString()
+    let burgerGroen = UIColor.init(red: 22/255, green: 191/255, blue: 173/255, alpha: 1.0)
     
     override func viewDidLoad() {
+        gebruikerIcon.image = UIImage(named: "Gebruiker")
+        wachtwoordIcon.image = UIImage(named: "Slotje")
+        sloganMutableString = NSMutableAttributedString(string: sloganString as String)
+        sloganMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: burgerGroen, range: NSRange(location:5,length:1))
+        sloganMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: burgerGroen, range: NSRange(location:13,length:1))
+        
+        sloganMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: burgerGroen, range: NSRange(location:19,length:1))
+        // set label Attribute
+        labelColours.attributedText = sloganMutableString
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
