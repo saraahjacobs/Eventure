d561d6ba7861ad5be84208f60822ffec62e6fcda
Sarah Jacobs <sarah.jacobs1@student.ehb.be>
2019-05-20 13:55:27 +0200

    Login + register

--- Eventure/RegisterViewController.swift
+++ Eventure/RegisterViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet var gebruikerIcon: UIImageView!
+    @IBOutlet var wachtwoordIcon: UIImageView!
+    @IBOutlet var emailIcon: UIImageView!
+    @IBOutlet var labelColours: UILabel!
+    
+    var sloganString:NSString = "enjoy. report. earn."
+    var sloganMutableString = NSMutableAttributedString()
+    let burgerGroen = UIColor.init(red: 22/255, green: 191/255, blue: 173/255, alpha: 1.0)
+    
     override func viewDidLoad() {
+        gebruikerIcon.image = UIImage(named: "Gebruiker")
+        wachtwoordIcon.image = UIImage(named: "Slotje")
+        emailIcon.image = UIImage(named: "Email")
+        sloganMutableString = NSMutableAttributedString(string: sloganString as String)
+        sloganMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: burgerGroen, range: NSRange(location:5,length:1))
+        sloganMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: burgerGroen, range: NSRange(location:13,length:1))
+        
+        sloganMutableString.addAttribute(NSAttributedString.Key.foregroundColor, value: burgerGroen, range: NSRange(location:19,length:1))
+        // set label Attribute
+        labelColours.attributedText = sloganMutableString
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
